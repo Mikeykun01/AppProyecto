80f441ab3c1f16ca15ca7113338fc11197ebaee6
Mikeykun01 <101691837+Mikeykun01@users.noreply.github.com>
2022-04-28 23:06:46 -0600

    si

--- app/src/main/java/app/bego/myapplication/MainActivity.java
+++ app/src/main/java/app/bego/myapplication/MainActivity.java
@@ -2,13 +2,44 @@ package app.bego.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button Btn3;
+    private TextView txt;
+    private Switch btns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Btn3 = findViewById(R.id.btn3);
+        txt = findViewById(R.id.estadotxt);
+        btns = findViewById(R.id.switch1);
+
+        Btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iThird = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(iThird);
+            }
+        });
+    }
+
+    public void onclick(View view) {
+        if (view.getId() == R.id.switch1) {
+            if (btns.isChecked()) {
+                txt.setText("Estado: Encendiste el botón :D");
+            } else {
+                txt.setText("Estado: Apagaste el botón :C");
+            }
+
+        }
     }
 }
--- app/src/main/java/app/bego/myapplication/ThirdActivity.java
+++ app/src/main/java/app/bego/myapplication/ThirdActivity.java
@@ -2,13 +2,70 @@ package app.bego.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ThirdActivity extends AppCompatActivity {
+    private Button BtnSocial, BtnCorreo, BtnCam, BtnCall;
+    private EditText Txtemail, Txtsocial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        //Botones
+        BtnSocial = findViewById(R.id.socialbtn);
+        BtnCorreo = findViewById(R.id.emailbtn);
+        BtnCam = findViewById(R.id.cambtn);
+        BtnCall = findViewById(R.id.callbtn);
+        //Edit Text
+        Txtemail = findViewById(R.id.emailtxt);
+        Txtsocial = findViewById(R.id.socialtxt);
+
+        BtnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iLlamada = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 911"));
+                startActivity(iLlamada);
+            }
+        });
+
+        BtnCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iCam = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(iCam);
+            }
+        });
+
+        BtnSocial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = Txtsocial.getText().toString();
+                if (url != null && !url.isEmpty()) {
+                    Intent intentWeb = new Intent();
+                    intentWeb.setAction(Intent.ACTION_VIEW);
+                    intentWeb.setData(Uri.parse("http://" + url));
+                    startActivity(intentWeb);
+                }
+            }
+        });
+
+        BtnCorreo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = Txtemail.getText().toString();
+                Intent iCorreoCompleto = new Intent(Intent.ACTION_SEND, Uri.parse("mailto: " + email));
+                iCorreoCompleto.setType("plain/text");
+                iCorreoCompleto.putExtra(Intent.EXTRA_SUBJECT, "Evaluación");
+                iCorreoCompleto.putExtra(Intent.EXTRA_TEXT, "Comprobación de que la app funciona correctamente.");
+                iCorreoCompleto.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+                startActivity(Intent.createChooser(iCorreoCompleto, "Elige cliente de correo"));
+            }
+        });
     }
 }
